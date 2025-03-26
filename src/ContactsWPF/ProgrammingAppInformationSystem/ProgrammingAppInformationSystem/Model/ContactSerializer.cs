8a7bdcc7a05f137c0f218fc4ee6c4c7dce507dc8
Nikomi0824 <karataevviacslav@gmail.com>
2025-03-27 00:15:07 +0700

    сделал лабу 3

---
+++ src/ContactsWPF/ProgrammingAppInformationSystem/ProgrammingAppInformationSystem/Model/ContactSerializer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+public static class ContactSerializer
+{
+    public static void Serialize(string filePath, IEnumerable<Contact> contacts)
+    {
+        var serializer = new XmlSerializer(typeof(List<Contact>));
+        using (var writer = new StreamWriter(filePath))
+        {
+            serializer.Serialize(writer, new List<Contact>(contacts));
+        }
+    }
+
+    public static List<Contact> Deserialize(string filePath)
+    {
+        var serializer = new XmlSerializer(typeof(List<Contact>));
+        using (var reader = new StreamReader(filePath))
+        {
+            return (List<Contact>)serializer.Deserialize(reader);
+        }
+    }
+}
